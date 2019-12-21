7c6bb5e5649ee16caf83f0d96692e4d289bb4335
WallLin <dinglin_sc@qq.com>
2019-12-21 11:09:15 +0800

    init repo

---
+++ src/test/java/cn/kyrie/miaosha/MiaoshaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.kyrie.miaosha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaoshaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
