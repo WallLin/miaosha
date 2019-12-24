1aa4ad86c2451dff87f5dac9e7604f2e09af40c4
WallLin <dinglin_sc@qq.com>
2019-12-24 20:45:27 +0800

    两次MD5: 实现MD5Util

---
+++ src/main/java/cn/kyrie/miaosha/util/MD5Util.java
@@ -0,0 +1,37 @@
+package cn.kyrie.miaosha.util;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+/**
+ * @author kyrie
+ * @date 2019-12-24 - 19:58
+ */
+public class MD5Util {
+
+    private static String md5(String str) {
+        return DigestUtils.md5Hex(str);
+    }
+
+    private static String salt = "1a2b3c4d"; // 固定盐
+
+    public static String inputPassToFormPass(String inputPass) {
+        String str = "" + salt.charAt(0) + salt.charAt(2) + inputPass + salt.charAt(5) + salt.charAt(4);
+        return md5(str);
+    }
+
+    public static String formPassToDBPass(String formPass, String salt) {
+        String str = "" + salt.charAt(0) + salt.charAt(2) + formPass + salt.charAt(5) + salt.charAt(4);
+        return md5(str);
+    }
+
+    public static String inputPassToDBPass(String inputPass, String salt) {
+        String formPass = inputPassToFormPass(inputPass);
+        return formPassToDBPass(formPass, salt);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(inputPassToFormPass("123456")); // d3b1294a61a07da9b49b6e22b2cbd7f9
+        System.out.println(formPassToDBPass(inputPassToFormPass("123456"), "1a2b3c4d")); // b7797cce01b4b131b433b6acf4add449
+        System.out.println(inputPassToDBPass("123456", "1a2b3c4d"));
+    }
+}
