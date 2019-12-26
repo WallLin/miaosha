52234deba579ab79c93feea8b7393b9013ba2f1f
WallLin <dinglin_sc@qq.com>
2019-12-26 17:16:27 +0800

    秒杀功能开发及管理后台: 商品详情页

--- src/main/java/cn/kyrie/miaosha/controller/GoodsController.java
+++ src/main/java/cn/kyrie/miaosha/controller/GoodsController.java
@@ -6,6 +6,7 @@ import cn.kyrie.miaosha.vo.GoodsVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,12 +24,40 @@ public class GoodsController {
     @Autowired
     GoodsService goodsService;
 
-
     @RequestMapping("/to_list")
     public String list(Model model, MiaoshaUser user) {
         model.addAttribute("user", user);
         List<GoodsVo> goodsList = goodsService.listGoodsVo();
         model.addAttribute("goodsList", goodsList);
         return "goods_list";
+    }
+
+    @RequestMapping("/to_detail/{goodsId}")
+    public String detail(Model model, MiaoshaUser user,
+                         @PathVariable(name = "goodsId") Long goodsId) {
+        model.addAttribute("user", user);
+        GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
+
+        long startAt = goods.getStartDate().getTime(); // 秒杀开始时间
+        long endAt = goods.getEndDate().getTime(); // 秒杀结束时间
+        long now = System.currentTimeMillis(); // 当前时间
+
+        int miaoshaStatus = 0; // '0':秒杀倒计时；'1':秒杀进行中；'2':秒杀已结束
+        int remainSeconds = 0; // 秒杀倒计时还剩多少秒
+
+        if (now < startAt) { // 秒杀还没开始，倒计时
+            miaoshaStatus = 0;
+            remainSeconds = (int) ((startAt - now) / 1000);
+        } else if (now > endAt) { // 秒杀已结束
+            miaoshaStatus = 2;
+            remainSeconds = -1;
+        } else { // 秒杀进行中
+            miaoshaStatus = 1;
+            remainSeconds = 0;
+        }
+        model.addAttribute("goods", goods);
+        model.addAttribute("miaoshaStatus", miaoshaStatus);
+        model.addAttribute("remainSeconds", remainSeconds);
+        return "goods_detail";
     }
 }
--- src/main/java/cn/kyrie/miaosha/dao/GoodsDao.java
+++ src/main/java/cn/kyrie/miaosha/dao/GoodsDao.java
@@ -2,6 +2,7 @@ package cn.kyrie.miaosha.dao;
 
 import cn.kyrie.miaosha.vo.GoodsVo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -13,6 +14,9 @@ import java.util.List;
 @Mapper
 public interface GoodsDao {
 
-    @Select("select g.*, mg.miaosha_price, mg.stock_count, mg.start_date, mg.end_date from miaosha_goods mg left outer join goods g on mg.id = g.id")
+    @Select("select g.*, mg.miaosha_price, mg.stock_count, mg.start_date, mg.end_date from miaosha_goods mg left join goods g on mg.id = g.id")
     List<GoodsVo> listGoodsVo();
+
+    @Select("select g.*, mg.miaosha_price, mg.stock_count, mg.start_date, mg.end_date from miaosha_goods mg left join goods g on mg.id = g.id where g.id = #{goodsId}")
+    GoodsVo getGoodsVoByGoodsId(@Param("goodsId") Long goodsId);
 }
