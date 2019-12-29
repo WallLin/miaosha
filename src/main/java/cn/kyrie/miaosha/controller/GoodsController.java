e2ac8f34a6150ed096869bf66b36a69142a7fef5
WallLin <dinglin_sc@qq.com>
2019-12-29 22:01:24 +0800

    页面缓存和URL缓存

--- src/main/java/cn/kyrie/miaosha/controller/GoodsController.java
+++ src/main/java/cn/kyrie/miaosha/controller/GoodsController.java
@@ -1,14 +1,23 @@
 package cn.kyrie.miaosha.controller;
 
 import cn.kyrie.miaosha.domain.MiaoshaUser;
+import cn.kyrie.miaosha.redis.GoodsKey;
+import cn.kyrie.miaosha.redis.RedisService;
 import cn.kyrie.miaosha.service.GoodsService;
 import cn.kyrie.miaosha.vo.GoodsVo;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.*;
 import java.util.List;
 
 /**
@@ -24,19 +33,72 @@ public class GoodsController {
     @Autowired
     GoodsService goodsService;
 
-    @RequestMapping("/to_list")
-    public String list(Model model, MiaoshaUser user) {
+    @Autowired
+    RedisService redisService;
+
+    @Autowired
+    ThymeleafViewResolver thymeleafViewResolver; // 手动渲染模板
+
+    /**
+     * Jmeter压测
+     *
+     * 2000 * 10
+     *
+     * 1.没有页面缓存
+     * error: 1)3.46%; 2)5.91%; 3)8.42%
+     *
+     * QPS: 1)450; 2)409; 3)462
+     *
+     * @param model
+     * @param user
+     * @return
+     */
+    @RequestMapping(value = "/to_list", produces = "text/html")
+    @ResponseBody
+    public String list(HttpServletRequest request, HttpServletResponse response,
+                       Model model, MiaoshaUser user) {
         model.addAttribute("user", user);
         List<GoodsVo> goodsList = goodsService.listGoodsVo();
         model.addAttribute("goodsList", goodsList);
-        return "goods_list";
+        //return "goods_list";
+
+        // 1、取缓存
+        String html = redisService.get(GoodsKey.getGoodsList, "", String.class);
+        if (!StringUtils.isEmpty(html)) {
+            return html;
+        }
+        // spring5
+        WebContext ctx = new WebContext(request, response, request.getServletContext(), request.getLocale(), model.asMap());
+        // 2、手动渲染模板
+        html = thymeleafViewResolver.getTemplateEngine().process("goods_list", ctx);
+        if (!StringUtils.isEmpty(html)) {
+            // 将页面放到缓存中
+            redisService.set(GoodsKey.getGoodsList, "", html);
+        }
+        return html;
     }
 
-    @RequestMapping("/to_detail/{goodsId}")
-    public String detail(Model model, MiaoshaUser user,
+    /**
+     * URL缓存
+     * @param model
+     * @param user
+     * @param goodsId
+     * @return
+     */
+    @RequestMapping(value = "/to_detail/{goodsId}", produces = "text/html")
+    @ResponseBody
+    public String detail(HttpServletRequest request, HttpServletResponse response,
+                         Model model, MiaoshaUser user,
                          @PathVariable(name = "goodsId") Long goodsId) {
         model.addAttribute("user", user);
         GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
+
+        // 1、取缓存
+        // 与页面缓存不一样的地方是：每个商品对应一个详情页面，因此每个详情页要单独存储，设置key时要加上商品的id
+        String html = redisService.get(GoodsKey.getGoodsDetail, "" + goodsId, String.class);
+        if (!StringUtils.isEmpty(html)) {
+            return html;
+        }
 
         long startAt = goods.getStartDate().getTime(); // 秒杀开始时间
         long endAt = goods.getEndDate().getTime(); // 秒杀结束时间
@@ -58,6 +120,15 @@ public class GoodsController {
         model.addAttribute("goods", goods);
         model.addAttribute("miaoshaStatus", miaoshaStatus);
         model.addAttribute("remainSeconds", remainSeconds);
-        return "goods_detail";
+        // return "goods_detail";
+        // spring5
+        WebContext ctx = new WebContext(request, response, request.getServletContext(), request.getLocale(), model.asMap());
+        // 2、手动渲染模板
+        html = thymeleafViewResolver.getTemplateEngine().process("goods_detail", ctx);
+        if (!StringUtils.isEmpty(html)) {
+            // 将页面放到缓存中
+            redisService.set(GoodsKey.getGoodsDetail, "" + goodsId, html);
+        }
+        return html;
     }
 }
---
+++ src/main/java/cn/kyrie/miaosha/redis/GoodsKey.java
@@ -0,0 +1,16 @@
+package cn.kyrie.miaosha.redis;
+
+/**
+ * @author kyrie
+ * @date 2019-12-23 - 23:20
+ */
+public class GoodsKey extends BasePrefix {
+
+    private GoodsKey(int expireSeconds, String prefix) {
+        super(expireSeconds, prefix);
+    }
+
+    // 页面缓存有效期设置为60s
+    public static GoodsKey getGoodsList = new GoodsKey(60,"gl");
+    public static GoodsKey getGoodsDetail = new GoodsKey(60,"gd");
+}
