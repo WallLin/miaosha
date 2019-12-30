91a58fed0a7a8ac55e1cb467b339e09a94eba9fa
WallLin <dinglin_sc@qq.com>
2019-12-30 23:07:10 +0800

    秒杀静态化

--- src/main/java/cn/kyrie/miaosha/controller/MiaoshaController.java
+++ src/main/java/cn/kyrie/miaosha/controller/MiaoshaController.java
@@ -3,7 +3,9 @@ package cn.kyrie.miaosha.controller;
 import cn.kyrie.miaosha.domain.MiaoshaOrder;
 import cn.kyrie.miaosha.domain.MiaoshaUser;
 import cn.kyrie.miaosha.domain.OrderInfo;
+import cn.kyrie.miaosha.exception.GlobalException;
 import cn.kyrie.miaosha.result.CodeMsg;
+import cn.kyrie.miaosha.result.Result;
 import cn.kyrie.miaosha.service.GoodsService;
 import cn.kyrie.miaosha.service.MiaoshaService;
 import cn.kyrie.miaosha.service.OrderService;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 秒杀控制器
@@ -38,12 +41,36 @@ public class MiaoshaController {
      *
      * QPS:  1)835; 2)1016; 3)1140;
      *
-     * @param model
      * @param user
      * @param goodsId
      * @return
      */
     @RequestMapping("/do_miaosha")
+    @ResponseBody
+    public Result<OrderInfo> doMiaosha(MiaoshaUser user, @RequestParam(name = "goodsId") long goodsId) {
+        if (user == null) {
+            return Result.error(CodeMsg.SESSION_ERROR);
+        }
+        // 判断库存
+        GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
+        int stockCount = goods.getStockCount();
+        if (stockCount <= 0) {
+            return Result.error(CodeMsg.MIAO_SHA_OVER);
+        }
+        // 判断该用户是否已经秒杀了，避免重复秒杀
+        MiaoshaOrder miaoshaOrder = orderService.getMiaoshaOrderByUserIdAndGoodsId(user.getId(), goodsId);
+        if (miaoshaOrder != null) {
+            return Result.error(CodeMsg.REPEATE_MIAOSHA);
+        }
+        // 减库存、下订单、写入秒杀订单
+        OrderInfo orderInfo = miaoshaService.miaosha(user, goods);
+        return Result.success(orderInfo);
+    }
+
+
+
+
+    /*@RequestMapping("/do_miaosha")
     public String doMiaosha(Model model, MiaoshaUser user,
                             @RequestParam(name = "goodsId") long goodsId) {
         if (user == null) {
@@ -67,5 +94,5 @@ public class MiaoshaController {
         model.addAttribute("orderInfo", orderInfo);
         model.addAttribute("goods", goods);
         return "order_detail";
-    }
+    }*/
 }
